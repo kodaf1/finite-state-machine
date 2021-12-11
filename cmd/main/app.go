bc948e22e4e31ca1c99e3c9ef154969bfd9af9e5
Mikhail Kodaf <contact@kodaf.me>
2021-12-12 01:54:24 +0300

    input file + formatted output

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"github.com/kodaf1/finite-state-machine/internal/fms"
+	"io"
 	"log"
 	"os"
+	"reflect"
 	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	cmd, err := reader.ReadString('\n')
+	data, err := os.Open("test.txt")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	cmd = strings.TrimSpace(cmd)
 
-	fmt.Println(fms.Run(cmd))
+	reader := bufio.NewReader(data)
+
+	for i := 1; ; i++ {
+		cmd, err := reader.ReadString('\n')
+		if err == io.EOF {
+			log.Println("EndOfFile")
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		cmd = strings.TrimSpace(cmd)
+
+		state, err := fms.Run(cmd)
+		log.Printf(
+			"%d test returns %t where last state is %v",
+			i,
+			err == nil && state.IsFinal(),
+			reflect.TypeOf(state))
+	}
 }
--- internal/fms/fms.go
+++ internal/fms/fms.go
@@ -2,17 +2,17 @@ package fms
 
 import "github.com/kodaf1/finite-state-machine/internal/fms/states"
 
-func Run(cmd string) bool {
+func Run(cmd string) (states.State, error) {
 	var state states.State
 	state = states.Start{}
 
 	for _, c := range cmd {
 		newState, err := state.Next(c)
 		if err != nil {
-			return false
+			return state, err
 		}
 		state = newState
 	}
 
-	return true
+	return state, nil
 }
